2a7aa404008a1cdcb96deb4908eae3b178700aec
shoptsc <sanusihamed@gmail.com>
2021-02-05 17:18:53 +0100

    updated

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const btn = document.querySelector('#btn');
 
-btn.addEventListener('click', (signin) => {
+btn.addEventListener('click', signin);
+
+function signin(e) {
     window.location.href = "signin.html"
-});
+}
